4fd508d1a593331e438e297e3c8c0e0db22d36e5
Kobrestad <kaare@obrestad.net>
2020-11-24 21:21:30 +0100

    fixed exponentialsmoothingoptimizer, added online learning to normal exponential smoothing

--- Source/ExponentialSmoothing.py
+++ Source/ExponentialSmoothing.py
@@ -1,57 +1,111 @@
+from matplotlib.pyplot import plot
 import DataLoading
 from Evaluation import Evaluation
 import numpy as np
 from matplotlib import pyplot as plt
-import pandas as pd
+
+WEEK = 167
+WEEK_PLUS_24 = 191
+
 
 def initial_trend(data, s_length):
     total = 0.0
     for i in range(s_length):
-        total += float(data[i+s_length] - data[i]) / s_length
+        total += float(data[i + s_length] - data[i]) / s_length
     return total / s_length
+
 
 def initial_seasonals(data, s_length):
     seasonals = {}
     season_averages = []
-    n_seasons = int(len(data)/s_length)
+    n_seasons = int(len(data) / s_length)
     # season averages
     for j in range(n_seasons):
         index = s_length * j
-        season_averages.append(sum(data[index:index+s_length])/float(s_length))
+        season_averages.append(sum(data[index : index + s_length]) / float(s_length))
     # initial values
     for i in range(s_length):
         sum_over_avg = 0.0
         for j in range(n_seasons):
-            sum_over_avg += data[s_length*j+i]-season_averages[j]
-        seasonals[i] = sum_over_avg/n_seasons
+            sum_over_avg += data[s_length * j + i] - season_averages[j]
+        seasonals[i] = sum_over_avg / n_seasons
     return seasonals
 
-def exponential_smoothing_triple(data, s_length=24, alpha=0.8, beta=0.025, gamma=0.90, n_preds=24):
+
+def exponential_smoothing_triple(
+    data, s_length=24, alpha=0.652, beta=0.028, gamma=0.932, n_preds=24
+):
     result = []
     seasonals = initial_seasonals(data, s_length)
-    for i in range(len(data)+n_preds):
-        if i == 0: # initial values
+    for i in range(len(data) + n_preds):
+        if i == 0:  # initial values
             smooth = data[0]
             trend = initial_trend(data, s_length)
             result.append(data[0])
             continue
-        if i >= len(data): # forecasting
+        if i >= len(data):  # forecasting
             m = i - len(data) + 1
-            result.append((smooth + m*trend) + seasonals[i%s_length])
-        else: # training values
+            result.append((smooth + m * trend) + seasonals[i % s_length])
+        else:  # training values
             val = data[i]
-            last_smooth, smooth = smooth, alpha*(val-seasonals[i%s_length]) + (1-alpha)*(smooth+trend)
-            trend = beta * (smooth-last_smooth) + (1-beta)*trend
-            seasonals[i%s_length] = gamma*(val-smooth) + (1-gamma)*seasonals[i%s_length]
-            result.append(smooth+trend+seasonals[i%s_length])
+            last_smooth, smooth = (
+                smooth,
+                alpha * (val - seasonals[i % s_length])
+                + (1 - alpha) * (smooth + trend),
+            )
+            trend = beta * (smooth - last_smooth) + (1 - beta) * trend
+            seasonals[i % s_length] = (
+                gamma * (val - smooth) + (1 - gamma) * seasonals[i % s_length]
+            )
+            result.append(smooth + trend + seasonals[i % s_length])
     return result
+
+
+def exponential_smoothing_triple_complete(
+    data,
+    test_data,
+    n_preds=WEEK_PLUS_24,
+):
+    y_train = data
+    y_test = test_data
+    y_total = np.append(y_train, y_test)
+    acc_res = []
+    done = False
+    i = 0
+    while not done:
+        i += 1
+        index = i * 24
+        if index > WEEK:
+            data_so_far = y_total[:index]
+            if y_total.size <= index:
+                done = True
+                break
+            y_pred = exponential_smoothing_triple(data=data_so_far, n_preds=n_preds)
+            next_day_one_week_forward = y_pred[WEEK + index : WEEK_PLUS_24 + index]
+            acc_res.extend(next_day_one_week_forward)
+
+    length = len(acc_res)
+    x_length = np.arange(length)
+    x_total_length = np.arange(len(y_total))
+    plt.figure("1", figsize=(15, 4))
+    plt.title("Predicted vs actual loss on test set")
+    plt.plot(x_length, acc_res, color="green", label="Predicted loss")
+    plt.plot(x_total_length, y_total, color="blue", label="Actual loss")
+    plt.xlabel(f"timer")
+    plt.ylabel(f"grid_loss")
+    plt.grid()
+    plt.legend(loc="best")
+
+    plt.show()
+    return acc_res
+
 
 def plot_results(y_train, y_test, y_predicted):
     x_total_length = len(y_train) + len(y_test)
     x_train = np.arange(len(y_train))
     x_test = np.arange(start=len(y_train), stop=x_total_length)
     x_pred = np.arange(start=len(y_train), stop=x_total_length)
-    plt.figure('1', figsize=(15, 3))
+    plt.figure("1", figsize=(15, 3))
     plt.title("Predicted vs actual loss on test set")
     plt.plot(x_train, y_train, color="green", label="Actual loss in training set")
     plt.plot(x_test, y_test, color="blue", label="Actual loss in test set")
@@ -59,18 +113,26 @@ def plot_results(y_train, y_test, y_predicted):
     plt.xlabel(f"timer")
     plt.ylabel(f"grid_loss")
     plt.grid()
-    plt.legend(loc='best')
+    plt.legend(loc="best")
 
     plt.show()
 
+
 def main():
-    (x_train, y_train), (x_test, y_test) = DataLoading.get_datasets("Data/raw/train.csv", "Data/raw/test.csv")
+    (x_train, y_train), (x_test, y_test) = DataLoading.get_datasets(
+        "Data/raw/train.csv", "Data/raw/test.csv"
+    )
+    y_total = np.append(y_train.values, y_test.values)
 
-    #print(y_train.tail())
-    #print(y_test.head())
-
-    y_pred = exponential_smoothing_triple(y_train.values, s_length=24 ,alpha=0.652, beta=0.028, gamma=0.932, n_preds=len(y_test))
-    y_predicted = y_pred[-len(y_test):]
+    y_pred = exponential_smoothing_triple(
+        y_train.values,
+        s_length=24,
+        alpha=0.652,
+        beta=0.028,
+        gamma=0.932,
+        n_preds=len(y_test),
+    )
+    y_predicted = y_pred[-len(y_test) :]
 
     # Evaluation array is mean_absolute_error, mean_squared_error, median_absolute_error respectively
     evaluation = Evaluation.run(y_test.values, y_predicted)
@@ -83,6 +145,13 @@ def main():
     print(f"Evaluation results for a next day a week into the future: {evaluation}")
 
     plot_results(y_train, y_test, y_predicted)
+
+    # Feed 24 hours more data at a time. Is better for total prediction, but less accurate on specifics.
+    y_pred = exponential_smoothing_triple_complete(y_train.values, y_test.values)
+
+    evaluation = Evaluation.run(y_total[len(y_total) - len(y_pred) :], y_pred)
+    print(f"Evaluation results from whole prediction: {evaluation}")
+
 
 if __name__ == "__main__":
     main()
